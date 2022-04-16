f298a416d159432b2abbd546bbf41a06c413ada1
tallbl0nde <40382856+tallbl0nde@users.noreply.github.com>
2022-04-16 16:37:38 +0930

    Move to cmake for building

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if building for Raspberry Pi
+if [ -f /etc/rpi-issue ]
+then
+  installArgs="-DRPI_BUILD=true"
+else
+  installArgs=""
+fi
+
+# Compile/install libraries first
+# AASDK
+pushd ./libs/aasdk > /dev/null
+mkdir -p build
+cd ./build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j$(nproc)
+sudo make install
+popd > /dev/null
+
+# OpenAuto
+pushd ./libs/openauto > /dev/null
+mkdir -p build
+cd ./build
+cmake -DCMAKE_BUILD_TYPE=Release ${installArgs} -DGST_BUILD=true ..
+make -j$(nproc)
+sudo make install
+popd > /dev/null
+
+# Build module
+mkdir -p build
+cd build
+cmake ..
+make -j$(nproc)
